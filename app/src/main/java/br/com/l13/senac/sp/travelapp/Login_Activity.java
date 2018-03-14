cd5721cf91b18ac471122938c5c9bb3d3b584a60
WILLIAN YOSHIMURA TADA <willian.ytada@senacsp.edu.br>
2018-03-14 11:04:04 -0300

    Initial commit

---
+++ app/src/main/java/br/com/l13/senac/sp/travelapp/Login_Activity.java
@@ -0,0 +1,13 @@
+package br.com.l13.senac.sp.travelapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
